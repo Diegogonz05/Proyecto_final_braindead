84d02811928c5601e503645e1fffd148fda64bfa
GGaboBD <007007gabo7u7@gmail.com>
2025-11-20 22:08:11 -0600

    Main

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
+#Main.py
+
 import tkinter 
 from UI import UI
-from tkinter import ttk
 
 
 
 # se guarda la funcion de ventana de tkinter en la variable ventana
 ventana = tkinter.Tk()
 
-
 # El tamaño de la ventana (px)
-ventana.geometry("300x600")
-
-ventana.configure(background= "#565758")
-tkinter.Wm.title(ventana, "NutriSense")
+ventana.geometry("500x550")
+ventana.title("NutriSense")
 
 
 UI(ventana)
